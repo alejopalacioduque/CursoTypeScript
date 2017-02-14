7529503489217ce1cfb245c3da35ef2526059b86
alejopalacioduque <alejopalacioduque@gmail.com>
2017-02-13 23:35:58 -0500

    commit 0002 - Tipos de datos en TypeScript

--- Prueba.js
+++ Prueba.js
@@ -4,7 +4,12 @@
 function holaMundo(nombre) {
     return "hola mundo !!" + nombre;
 }
-var nombre = 'Alejo..';
+var nombre = "Alejo";
 var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById("container");
 etiqueta.innerHTML = resultado;
+// variables y tipos
+var edad = 619;
+var programador = true;
+var lenguajes = ["java", "SQL", "TypeScript"];
+etiqueta.innerHTML = nombre + " - " + edad + " - " + lenguajes[1];
--- Prueba.ts
+++ Prueba.ts
@@ -5,8 +5,18 @@ function holaMundo(nombre){
     return "hola mundo !!" + nombre;
 }
 
-var nombre = "Alejo";
+var nombre : string = "Alejo";
 var resultado = holaMundo(nombre);
 
 var etiqueta = <HTMLElement> document.getElementById("container");
 etiqueta.innerHTML = resultado;
+
+// variables y tipos
+
+var edad : number = 619;
+var programador : boolean = true;
+var lenguajes : Array<string> = ["java", "SQL", "TypeScript"];
+
+etiqueta.innerHTML = nombre + " - " + edad + " - "+lenguajes[1];
+
+
