585fc02ddc1b861df8fee7e0c83a660e9e6449f6
alejopalacioduque <alejopalacioduque@gmail.com>
2017-02-13 23:43:05 -0500

    commit 0003 - variables tipo let - var TypeScript

--- Prueba.js
+++ Prueba.js
@@ -13,3 +13,11 @@ var edad = 619;
 var programador = true;
 var lenguajes = ["java", "SQL", "TypeScript"];
 etiqueta.innerHTML = nombre + " - " + edad + " - " + lenguajes[1];
+var a = 7;
+var b = 12;
+if (a = 7) {
+    var a_1 = 4;
+    var b = 1;
+    console.log("Dentro del if -->" + a_1 + " - " + b);
+}
+console.log("Fuera del if -->" + a + " - " + b);
--- Prueba.ts
+++ Prueba.ts
@@ -19,4 +19,15 @@ var lenguajes : Array<string> = ["java", "SQL", "TypeScript"];
 
 etiqueta.innerHTML = nombre + " - " + edad + " - "+lenguajes[1];
 
+var a = 7;
+var b = 12
+
+if(a = 7){
+    let a = 4;
+    var b = 1;
+    console.log("Dentro del if -->" + a +" - " + b)
+}
+
+console.log("Fuera del if -->" + a +" - " + b)
+
 
